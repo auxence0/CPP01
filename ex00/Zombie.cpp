e3023c4f625c8b390ad8ef0f1d432c68ab72ba0a
auxence0 <sauvagetauxence@gmail.com>
2026-07-05 17:46:56 +0200

    fixes

--- ex00/Zombie.cpp
+++ ex00/Zombie.cpp
@@ -6,13 +6,13 @@
 /*   By: asauvage <asauvage@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/05 16:25:49 by asauvage          #+#    #+#             */
-/*   Updated: 2026/07/05 16:48:09 by asauvage         ###   ########.fr       */
+/*   Updated: 2026/07/05 17:23:41 by asauvage         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Zombie.hpp"
 
-Zombie::Zombie ( std::string set_name ) :name(set_name) {
+Zombie::Zombie ( std::string set_name ): name(set_name) {
 	return ;
 }
 
