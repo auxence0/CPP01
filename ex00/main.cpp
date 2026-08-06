05cc49ea91adc8c7563a1ca537224d7e48b2dda3
auxence <auxence@macbook-air-de-auxence.home>
2026-08-06 19:20:15 +0200

    name zombstack

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,14 +6,14 @@
 /*   By: asauvage <asauvage@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/05 16:45:48 by asauvage          #+#    #+#             */
-/*   Updated: 2026/07/05 16:52:01 by asauvage         ###   ########.fr       */
+/*   Updated: 2026/08/06 19:19:20 by asauvage         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Zombie.hpp"
 
 int	main () {
-	randomChump("Zombite");
+	randomChump("Zombstack");
 
 	Zombie*	zomb = newZombie("Zombheaper");
 	zomb->announce();
