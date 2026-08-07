75b2a8d5d437f0172bb59388151256731b6972b3
auxence0 <sauvagetauxence@gmail.com>
2026-08-07 12:50:08 +0200

    modif res value *stringPTR

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: asauvage <asauvage@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/05 17:33:40 by asauvage          #+#    #+#             */
-/*   Updated: 2026/07/05 17:46:28 by asauvage         ###   ########.fr       */
+/*   Updated: 2026/08/07 12:49:20 by asauvage         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,6 @@ int	main () {
 	std::cout << &stringREF << "\n";
 
 	std::cout << str << "\n";
-	std::cout << stringPTR << "\n";
+	std::cout << *stringPTR << "\n";
 	std::cout << stringREF << "\n";
-}	
+}
